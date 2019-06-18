926e33dc3612281a406542d767fa06b1e5b4ad3b
rubeeengm <rubeeengm@gmail.com>
2019-06-18 15:12:55 -0500

    clase sound añadida a cada uno de los enlaces de audio del pad, evento para reproducir sonido de los pads creado

---
+++ main.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", () => {
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+
+    //console.log(sounds)
+
+    //Lets get going with the sound here
+    pads.forEach((pad, index) => {
+        pad.addEventListener('click', function() {
+            sounds[index].play();
+        });
+    });
+});
